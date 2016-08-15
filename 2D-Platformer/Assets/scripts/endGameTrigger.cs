081458529f4c0c0ea5d1e07e1b20a7156d20d68e
Phillip Chuzhbinin <phillip@coffeemeetsbagel.com>
2016-08-14 19:54:11 -0700

    New changes to L2 on 2D Platformer

--- 2D-Platformer/Assets/scripts/endGameTrigger.cs
+++ 2D-Platformer/Assets/scripts/endGameTrigger.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class endGameTrigger : MonoBehaviour {
 	public int levelNum;
+	public bool finalLevel;
+	public GameObject playAgainButton;
 	// Use this for initialization
 	void Start () {
 	
@@ -14,8 +16,15 @@ public class endGameTrigger : MonoBehaviour {
 	}
 	void OnTriggerEnter2D(Collider2D other){
 		if(other.tag == "Player"){
+			if (finalLevel) {
+				playAgainButton.SetActive (true);
+			} else {
 			Application.LoadLevel ("Level" + levelNum);
+			}
 		}
 
+	}
+	public void restartGame(){
+		Application.LoadLevel ("Level1");
 	}
 }
