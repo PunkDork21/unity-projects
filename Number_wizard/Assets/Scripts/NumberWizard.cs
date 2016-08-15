466c66019dafe0c74867feaefaf6658066d995a8
Phillip Chuzhbinin <phillip@coffeemeetsbagel.com>
2016-08-15 09:06:18 -0700

    Number_wizard game started

---
+++ Number_wizard/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Welcome to Number Wizard");
+		print ("Pick a number, any number BUT DON'T TELL ME!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
