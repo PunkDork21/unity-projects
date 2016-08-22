69c2af9066011541d7a3bbcc302cf1974e62d846
Phillip Chuzhbinin <phillip@coffeemeetsbagel.com>
2016-08-22 10:00:22 -0700

    Number wizard changes

--- Number_wizard/Assets/Scripts/NumberWizard.cs
+++ Number_wizard/Assets/Scripts/NumberWizard.cs
@@ -7,10 +7,26 @@ public class NumberWizard : MonoBehaviour {
 	void Start () {
 		print ("Welcome to Number Wizard");
 		print ("Pick a number, any number BUT DON'T TELL ME!");
+
+		int max = 1000;
+		int min = 1;
+
+		print ("The highest number you can choose is " + max);
+		print ("The lowest number you can choose is " + min);
+
+		print("Is the number higher than or lower than 500?");
+		print("Up = higher, down = lower, return = equal");
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			print("That's up mate");
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			print("Dats down mate");
+		}
 	}
 }
