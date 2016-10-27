67d7e13546d08ce4d7e5cf505b04b2183deafba5
Phillip Chuzhbinin <phillip@coffeemeetsbagel.com>
2016-10-27 13:32:15 -0700

    Scripts and changes

---
+++ Escape_the_dating_scene/Assets/Scripts/moneyController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class moneyController : MonoBehaviour {
+
+	public Text money;
+	public static int cash;
+
+	// Use this for initialization
+	void Start () {
+		cash = 900;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		money.text = "$" + cash;
+	}
+	void takeMoney(){
+
+	}
+}
+
+
+	
