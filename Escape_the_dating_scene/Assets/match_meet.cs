2266fc8892ac938353711717b46321f6116a1d8f
Nicolas Vayias <nicolas@coffeemeetsbagel.com>
2016-10-27 13:47:37 -0700

    roads added + destroyed objs

---
+++ Escape_the_dating_scene/Assets/match_meet.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class match_meet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	void OnCollisionEnter(Collision col) {
+
+		if (col.collider.name == "RigidBodyFPSController") {
+			Destroy(gameObject);
+		}
+////		Destroy(gameObject);
+//
+//		print("Detected collision between " + gameObject.name + " and " + collisionInfo.collider.name);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
